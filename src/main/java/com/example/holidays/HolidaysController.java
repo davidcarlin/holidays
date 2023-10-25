bc327a45acd98b94be95c26ebc2b568fe20807ae
davidcarlin <david.carlin@barclays.com>
2023-10-26 00:14:21 +0100

    added index.html + altered controller to handle root

--- src/main/java/com/example/holidays/HolidaysController.java
+++ src/main/java/com/example/holidays/HolidaysController.java
@@ -2,19 +2,36 @@ package com.example.holidays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/")
 public class HolidaysController {
 
+    private final ResourceLoader resourceLoader;
     private final HolidaysService holidaysService;
 
     @Autowired
-    public HolidaysController(HolidaysService holidaysService) {
+    public HolidaysController(ResourceLoader resourceLoader, HolidaysService holidaysService) {
+        this.resourceLoader = resourceLoader;
         this.holidaysService = holidaysService;
+    }
+
+    @GetMapping("/")
+    public String index() throws IOException {
+        // Load and return the content of index.html
+        Resource resource = resourceLoader.getResource("classpath:static/index.html");
+        try (InputStream inputStream = resource.getInputStream()) {
+            byte[] bytes = StreamUtils.copyToByteArray(inputStream);
+            return new String(bytes, StandardCharsets.UTF_8);
+        }
     }
 
     @PostMapping("/calculate")
@@ -26,6 +43,4 @@ public class HolidaysController {
         String result = holidaysService.calculateHolidays(targetColleague, totalHolidayAllowance, fileUrl);
         return ResponseEntity.ok(result);
     }
-
-
 }
